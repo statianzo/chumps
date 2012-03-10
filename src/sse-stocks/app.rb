656b97ed82478921b38139210052022e09c04cfa
Jason Staten <jstaten07@gmail.com>
2012-03-09 17:51:05 -0700

    more work

--- src/sse-stocks/app.rb
+++ src/sse-stocks/app.rb
@@ -1,26 +1,35 @@
 require 'bundler/setup'
 Bundler.require
+Thread.abort_on_exception = true
 
-$stocks = {CAKE: 100.0, QQ: 50.0, LOL: 85.0}
-$connections = []
-set server: 'thin'
+set server: 'thin',
+    conns: [],
+    stocks: {AAPL: 500.0, MSFT: 30.0, GOOG: 600.0}
+
+get '/' do
+  erb :index, locals: { stocks: settings.stocks }
+end
+
+get '/stocks', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    settings.conns.push(out)
+    out.callback { settings.conns.delete(out) }
+  end
+end
+
+def notify(data, name=nil)
+  settings.conns.each do |out|
+    out << "event: #{name}\n" if name
+    out << "data: #{data}\n\n"
+  end
+end
 
 Thread.new do
   while true
     sleep rand(2)
-    k = $stocks.keys[rand($stocks.length)]
-    $stocks[k] += 5 - rand(10)
-    $connections.each {|c| c << "event: #{k}\ndata: #{$stocks[k]}\n\n"}
-  end
-end
-
-get '/' do
-  erb :index, locals: { stocks: $stocks }
-end
-
-get '/stream', provides: 'text/event-stream' do
-  stream :keep_open do |out|
-    $connections << out
-    out.callback { $connections.delete(out) }
+    symbol = settings.stocks.keys.sample
+    settings.stocks[symbol] += rand(-5..5)
+    notify(settings.stocks[symbol], symbol)
+    notify("#{rand(2) == 1 ? 'Buy' : 'Sell'} Now!")
   end
 end
