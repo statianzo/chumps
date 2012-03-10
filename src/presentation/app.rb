f91947158973031fbddd92975b06cd6f8f99fde1
Jason Staten <jstaten07@gmail.com>
2012-03-10 07:58:53 -0700

    slidedeck good

--- src/presentation/app.rb
+++ src/presentation/app.rb
@@ -1,3 +1,5 @@
+ENV['masteruser'] ||= 'master'
+ENV['masterpass'] ||= 'pass'
 require 'bundler/setup'
 Bundler.require
 
@@ -5,10 +7,16 @@ set server: 'thin',
     connections: []
 
 get '/' do
-  erb :index, locals: { master: !!params['master'] }
+  erb :index, locals: { master: false }
+end
+
+get '/master' do
+  protected!
+  erb :index, locals: { master: true }
 end
 
 post '/navigate/:slide' do |slide|
+  protected!
   settings.connections.each {|c| c << "data: #{slide}\n\n" }
   204
 end
@@ -17,5 +25,19 @@ get '/view', provides: 'text/event-stream' do
   stream :keep_open do |out|
     settings.connections << out
     out.callback { settings.connections.delete(out) }
+  end
+end
+
+helpers do
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ENV['masteruser'], ENV['masterpass']]
   end
 end
